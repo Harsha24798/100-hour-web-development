f108fc47ba9caf9c71e901949c5b43280901d2ac
Harsha Madushan <69555530+Harsha24798@users.noreply.github.com>
2026-01-05 19:59:36 +0530

    first commit

---
+++ mernn-crash-course/backend/server.js
@@ -0,0 +1,16 @@
+import dotenv from "dotenv";
+import express from "express";
+import { connectDB } from "./config/db.js";
+
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(PORT, () => {
+  connectDB();
+  console.log(`Server is running on port ${PORT}`);
+});
