8c58dfe9ac77e8dbd7f9ba25d452396f8d21b652
Harsha Madushan <69555530+Harsha24798@users.noreply.github.com>
2026-01-05 20:50:11 +0530

    Project 01: complete crud

---
+++ mernn-crash-course/backend/controller/product.controller.js
@@ -0,0 +1,70 @@
+import Product from "../models/product.model.js";
+
+export const getAllProducts = async (req, res) => {
+  try {
+    const products = await Product.find({});
+
+    res.status(200).json({ success: true, data: products });
+  } catch (error) {
+    res.status(500).json({ success: false, message: "Server Error" });
+  }
+};
+
+export const createProduct = async (req, res) => {
+  const product = req.body;
+
+  if (!product || !product.name || !product.price) {
+    return res.status(400).json({ message: "Please provide all product data" });
+  }
+
+  const newProduct = new Product(product);
+
+  try {
+    await newProduct.save();
+
+    res.status(201).json({ success: true, data: newProduct });
+  } catch (error) {
+    res.status(500).json({ success: false, message: "Server Error" });
+  }
+};
+
+export const deleteProduct = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const deletedProduct = await Product.findByIdAndDelete(id);
+
+    if (!deletedProduct) {
+      return res
+        .status(404)
+        .json({ success: false, message: "Product not found" });
+    }
+
+    res
+      .status(200)
+      .json({ success: true, message: "Product deleted successfully" });
+  } catch (error) {
+    res.status(500).json({ success: false, message: "Server Error" });
+  }
+};
+
+export const updateProduct = async (req, res) => {
+  const { id } = req.params;
+  const updates = req.body;
+
+  try {
+    const updatedProduct = await Product.findByIdAndUpdate(id, updates, {
+      new: true,
+    });
+
+    if (!updatedProduct) {
+      return res
+        .status(404)
+        .json({ success: false, message: "Product not found" });
+    }
+
+    res.status(200).json({ success: true, data: updatedProduct });
+  } catch (error) {
+    res.status(500).json({ success: false, message: "Server Error" });
+  }
+};
--- mernn-crash-course/backend/server.js
+++ mernn-crash-course/backend/server.js
@@ -1,7 +1,7 @@
 import dotenv from "dotenv";
 import express from "express";
 import { connectDB } from "./config/db.js";
-import Product from "./models/product.model.js";
+import productrouter from "./routes/product.route.js";
 
 dotenv.config();
 const app = express();
@@ -9,68 +9,9 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 
-app.post("/api/products", async (req, res) => {
-  const product = req.body;
+app.use("/api/products", productrouter);
 
-  if(!product || !product.name || !product.price) {
-    return res.status(400).json({ message: "Please provide all product data" });
-  }
 
-  const newProduct = new Product(product);
-
-  try {
-    await newProduct.save();
-
-    res.status(201).json({ success: true, data: newProduct });
-  } catch (error) {
-    res.status(500).json({ success: false, message: "Server Error" });
-  }
-});
-
-app.delete("/api/products/:id", async (req, res) => {
-
-  const { id } = req.params;
-  
-  try {
-    const deletedProduct = await Product.findByIdAndDelete(id);
-
-    if (!deletedProduct) {
-      return res.status(404).json({ success: false, message: "Product not found" });
-    }
-    
-    res.status(200).json({ success: true, message: "Product deleted successfully" });
-  } catch (error) {
-    res.status(500).json({ success: false, message: "Server Error" });
-  }
-});
-
-app.get("/api/products", async (req, res) => {
-  try {
-    const products = await Product.find({});
-
-    res.status(200).json({ success: true, data: products });
-  } catch (error) {
-    res.status(500).json({ success: false, message: "Server Error" });
-  }
-});
-
-app.put("/api/products/:id", async (req, res) => {
-
-  const { id } = req.params;
-  const updates = req.body;
-
-  try {
-    const updatedProduct = await Product.findByIdAndUpdate(id, updates, { new: true });
-
-    if (!updatedProduct) {
-      return res.status(404).json({ success: false, message: "Product not found" });
-    }
-
-    res.status(200).json({ success: true, data: updatedProduct });
-  } catch (error) {
-    res.status(500).json({ success: false, message: "Server Error" });
-  }
-});
 
 app.listen(PORT, () => {
   connectDB();
