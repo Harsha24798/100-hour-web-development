1583ca949a9033e2e6594add0cce18ca5e19c31a
Harsha Madushan <69555530+Harsha24798@users.noreply.github.com>
2026-01-05 20:22:11 +0530

    create first api and store mongo bd database

--- mernn-crash-course/backend/server.js
+++ mernn-crash-course/backend/server.js
@@ -1,13 +1,29 @@
 import dotenv from "dotenv";
 import express from "express";
 import { connectDB } from "./config/db.js";
+import Product from "./models/product.model.js";
 
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+app.use(express.json());
+
+app.post("/api/products", async (req, res) => {
+  const product = req.body;
+
+  if(!product || !product.name || !product.price) {
+    return res.status(400).json({ message: "Please provide all product data" });
+  }
+
+  const newProduct = new Product(product);
+
+  try {
+    await newProduct.save();
+    res.status(201).json({ success: true, data: newProduct });
+  } catch (error) {
+    res.status(500).json({ success: false, message: "Server Error" });
+  }
 });
 
 app.listen(PORT, () => {
