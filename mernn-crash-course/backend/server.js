638ee37e77f8fc1106f34511a9a3d736cc79c69a
Harsha Madushan <69555530+Harsha24798@users.noreply.github.com>
2026-01-05 20:35:07 +0530

    Project 01: create update, getall api

--- mernn-crash-course/backend/server.js
+++ mernn-crash-course/backend/server.js
@@ -20,6 +20,7 @@ app.post("/api/products", async (req, res) => {
 
   try {
     await newProduct.save();
+
     res.status(201).json({ success: true, data: newProduct });
   } catch (error) {
     res.status(500).json({ success: false, message: "Server Error" });
@@ -32,10 +33,40 @@ app.delete("/api/products/:id", async (req, res) => {
   
   try {
     const deletedProduct = await Product.findByIdAndDelete(id);
+
     if (!deletedProduct) {
       return res.status(404).json({ success: false, message: "Product not found" });
     }
+    
     res.status(200).json({ success: true, message: "Product deleted successfully" });
+  } catch (error) {
+    res.status(500).json({ success: false, message: "Server Error" });
+  }
+});
+
+app.get("/api/products", async (req, res) => {
+  try {
+    const products = await Product.find({});
+
+    res.status(200).json({ success: true, data: products });
+  } catch (error) {
+    res.status(500).json({ success: false, message: "Server Error" });
+  }
+});
+
+app.put("/api/products/:id", async (req, res) => {
+
+  const { id } = req.params;
+  const updates = req.body;
+
+  try {
+    const updatedProduct = await Product.findByIdAndUpdate(id, updates, { new: true });
+
+    if (!updatedProduct) {
+      return res.status(404).json({ success: false, message: "Product not found" });
+    }
+
+    res.status(200).json({ success: true, data: updatedProduct });
   } catch (error) {
     res.status(500).json({ success: false, message: "Server Error" });
   }
