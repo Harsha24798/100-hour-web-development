8bc044eda086a8040bba7752b0fb99cbf0cd1e1f
Harsha Madushan <69555530+Harsha24798@users.noreply.github.com>
2026-01-06 22:05:06 +0530

    Project 01: setup react router

--- mernn-crash-course/frontend/src/App.jsx
+++ mernn-crash-course/frontend/src/App.jsx
@@ -1,12 +1,19 @@
-import { Button } from "@chakra-ui/react"
+import { Route, Routes } from "react-router-dom";
+import { Box } from "@chakra-ui/react";
+import HomePage from "./pages/HomePage";
+import CreatePage from "./pages/CreatePage";
+import Navbar from "./components/Navbar";
 
 function App() {
-
   return (
-    <>
-      <Button>Click Me</Button>
-    </>
-  )
+    <Box minH={"100vh"}>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/create" element={<CreatePage />} />
+      </Routes>
+    </Box>
+  );
 }
 
-export default App
+export default App;
