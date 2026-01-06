e225eb152fe8405afdf18f7563aaef47feae0c06
Harsha Madushan <69555530+Harsha24798@users.noreply.github.com>
2026-01-06 22:30:34 +0530

    Project 01: navbar setup with light and dark mod

--- mernn-crash-course/frontend/src/components/Navbar.jsx
+++ mernn-crash-course/frontend/src/components/Navbar.jsx
@@ -1,7 +1,47 @@
-import React from "react";
+import { Button, Container, Flex, HStack, Text } from "@chakra-ui/react";
+import { Link } from "react-router-dom";
+import { FiPlusSquare } from "react-icons/fi";
+import { IoMoon } from "react-icons/io5";
+import { LuSun } from "react-icons/lu";
+import { useColorMode } from "@/components/ui/color-mode";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  const { colorMode, toggleColorMode } = useColorMode();
+
+  return (
+    <Container maxW={"1140px"} px={4}>
+      <Flex
+        py={4}
+        justifyContent={"space-between"}
+        alignItems={"center"}
+        flexDir={{ base: "column", sm: "row" }}
+      >
+        <Text
+          fontSize={{ base: "22", sm: "28" }}
+          fontWeight={"bold"}
+          textTransform={"uppercase"}
+          textAlign={"center"}
+          bgGradient="to-r"
+          gradientFrom="cyan.400"
+          gradientTo="blue.500"
+          backgroundClip="text"
+        >
+          <Link to="/">Product Store 🏬</Link>
+        </Text>
+
+        <HStack spacing={2} alignItems={"center"}>
+          <Link to="/create">
+            <Button>
+              <FiPlusSquare fontSize="20px" />
+            </Button>
+          </Link>
+          <Button onClick={toggleColorMode}>
+            {colorMode === "light" ? <IoMoon /> : <LuSun />}
+          </Button>
+        </HStack>
+      </Flex>
+    </Container>
+  );
 };
 
 export default Navbar;
