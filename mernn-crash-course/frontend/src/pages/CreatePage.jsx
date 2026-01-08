63894b5d9b43bc0403e382c51d06af543b26e48d
Harsha Madushan <69555530+Harsha24798@users.noreply.github.com>
2026-01-08 15:53:54 +0530

    Project  01: refactor CreatePage component to manage product creation with form inputs

--- mernn-crash-course/frontend/src/pages/CreatePage.jsx
+++ mernn-crash-course/frontend/src/pages/CreatePage.jsx
@@ -1,7 +1,73 @@
+import {
+  Box,
+  Button,
+  Container,
+  Heading,
+  Input,
+  VStack,
+} from "@chakra-ui/react";
 import React from "react";
+import { useColorModeValue } from "@/components/ui/color-mode";
 
 const CreatePage = () => {
-  return <h1>CreatePage</h1>;
+  const [newProduct, setNewProduct] = React.useState({
+    name: "",
+    price: "",
+    image: "",
+  });
+
+  const handleAddProduct = () => {
+    console.log(newProduct);
+  };
+
+  return (
+    <Container maxW={"container.sm"}>
+      <VStack spacing={8}>
+        <Heading as={"h1"} size={"2xl"} textAlign={"center"} mb={8}>
+          Create New Product
+        </Heading>
+
+        <Box
+          w={"full"}
+          bg={useColorModeValue("white", "gray.800")}
+          p={6}
+          rounded={"lg"}
+          shadow={"md"}
+        >
+          <VStack spacing={4}>
+            <Input
+              placeholder="Product Name"
+              name="name"
+              value={newProduct.name}
+              onChange={(e) =>
+                setNewProduct({ ...newProduct, name: e.target.value })
+              }
+            />
+            <Input
+              placeholder="Product Price"
+              name="price"
+              value={newProduct.price}
+              onChange={(e) =>
+                setNewProduct({ ...newProduct, price: e.target.value })
+              }
+            />
+            <Input
+              placeholder="Product Image URL"
+              name="image"
+              value={newProduct.image}
+              onChange={(e) =>
+                setNewProduct({ ...newProduct, image: e.target.value })
+              }
+            />
+
+            <Button colorScheme={"blue"} onClick={handleAddProduct} w={"full"}>
+              Add Product
+            </Button>
+          </VStack>
+        </Box>
+      </VStack>
+    </Container>
+  );
 };
 
 export default CreatePage;
