4230846ca2c2db6badc0b7a61bf8cd67580b2cb7
Harsha24798 <harshamadushan24798@gmail.com>
2026-01-19 09:45:49 +0530

    Project 02: implement chat functionality with Sidebar, ChatContainer, and NoChatSelected components; add Zustand store for state management

--- chat-app/backend/src/index.js
+++ chat-app/backend/src/index.js
@@ -11,8 +11,8 @@ const app = express();
 
 const PORT = process.env.PORT || 5000;
 
-app.use(express.json({ limit: '10mb' }));
-app.use(express.urlencoded({ limit: '10mb', extended: true }));
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ limit: "10mb", extended: true }));
 app.use(cookieParser());
 app.use(
   cors({
